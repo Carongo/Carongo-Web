a6df641091280e77ae84cd43bfe5061f0a0472e4
Henrique Leandro <miniqui10@gmail.com>
2021-03-16 02:55:19 -0300

    Home finalizada, falta só implementar header e footer

---
+++ src/pages/home/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Carousel, Container, Row, Col} from 'react-bootstrap';
+import './index.css'
+
+import carousel1 from '../../assets/img/Carrousel1.png'
+import carousel2 from "../../assets/img/Carrousel2.png";
+import eurekinha from "../../assets/img/eurekinha.png";
+
+
+
+const Home = () => {
+    return (
+        <div>
+          <Carousel fluid>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={carousel1}
+      alt="First slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={carousel2}
+      alt="Second slide"
+    />
+
+  </Carousel.Item>
+
+</Carousel>
+<div className="container">
+  
+
+
+
+
+<Container>
+  <Row>
+    <Col sm={5}><h1>Quem somos?</h1><p>Fazemos conexões, mas acima de tudo, somos a conexão. Somos o elo entre o que você vê por aí e o que você quer enxergar daqui para frente. A ponte entre a sua rotina e as suas ideias. Unimos a experiência profissional e a formação, a especialização e a vivência. Somos o que você lê para se inspirar, todas as regras e as mudanças. Somos a informação que move você, o mercado e o mundo. O conteúdo que comunica, explica e emociona. Somos completos. E completamos. Somos o que faz o mercado pulsar. A inteligência que traz a inovação do amanhã. Fazemos você e os resultados da sua marca crescerem. Juntos. Somos a paixão que faz você manter o brilho nos olhos a cada novo job. Somos o orgulho de ser, somos uma nação. Mais que um lugar qualquer, somos um mundo. O mundo por trás do marketing. Somos Mundo do Marketing. Você conectado com o mercado.</p></Col>
+ 
+
+
+    <Col sm={7}><img src={eurekinha}></img></Col>
+  </Row>
+  
+</Container>
+
+
+</div>
+
+        </div>
+    )
+}
+
+
+
+export default Home;
